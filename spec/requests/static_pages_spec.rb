050f694aa2d80d671152a948841c3851494368df
Maria Paschevska <mpasch@softserveinc.com>
2013-11-27 11:24:31 +0200

    Now I will rm support-utilities and create it againg. All tests before Listing 5.26 works

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,24 +10,14 @@ describe "Static pages" do
   #   it { should have_selector('title', text: full_title('')) }  
   # end
 
+  subject { page }
+
   describe "Home page" do
     before { visit root_path }
-
-    it "should have the h1 'Sample App'" do
-     
-      page.should have_selector('h1', text: 'Sample App')
-    end
-
-    it "should have the base title" do
-      
-      page.should have_selector('title',
-                        text: "Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      
-      page.should_not have_selector('title', text: '| Home')
-    end
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector 'title',
+                        text: "Ruby on Rails Tutorial Sample App" }
+    it { should_not have_selector 'title', text: '| Home' } 
   end
 
   describe "Help page" do
