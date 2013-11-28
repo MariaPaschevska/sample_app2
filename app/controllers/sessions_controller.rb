fe466a6e8e1233f2286c659f72c0e38434e1bd7d
Maria Paschevska <mpasch@softserveinc.com>
2013-11-28 17:15:02 +0200

    the test suite for users with invalid information OK. Need to make successful signin.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,16 @@ class SessionsController < ApplicationController
 
   def create
   	# for tets
-  	# binding.pry    
+  	# binding.pry 
+  	user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    # Sign the user in and redirect to the user's show page.
+
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+       # Create an error message and re-render the signin form.
+      render 'new'
+    end
   end
 
   def destroy
