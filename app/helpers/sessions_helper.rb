ab1e3936eef55914d652ac2198a14faab00449fe
Maria Paschevska <mpasch@softserveinc.com>
2013-11-29 11:48:23 +0200

    the signup/signin/signout completed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,31 @@
 module SessionsHelper
+	def sign_in(user)
+	#to place a remember token as a cookie on the user’s browser, and then use the token to find the user record in the database as the user moves from page to page 
+    cookies.permanent[:remember_token] = user.remember_token   
+    #to create current_user, accessible in both controllers and views 
+    self.current_user = user
+  end
+
+  #a user is signed in if current_user is not nil
+  def signed_in?
+    !current_user.nil?
+  end
+
+  #defines a method current_user= 
+  def current_user=(user)
+  	#sets an instance variable @current_user, effectively storing the user for later use
+    @current_user = user
+  end
+
+  def current_user
+  	#to set the @current_user instance variable to the user corresponding to the remember token, but only if @current_user is undefined
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  def sign_out
+  	#set the current user to nil 
+    self.current_user = nil
+    #use the delete method on cookies to remove the remember token from the session
+    cookies.delete(:remember_token)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
 
   # before_save { |user| user.email = email.downcase }
   before_save { email.downcase! }
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -23,5 +24,10 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  private   #All methods defined in a class after private are automatically hidden
   
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64   #self обеспечивает установку назначением пользовательского remember_token - он будет записан в DB вместе с другими атрибутами при сохранении пользователя.
+    end  
 end
