53516eb39523fe38d8eb689a5e8dbcee7796c5a5
Maria Paschevska <mpasch@softserveinc.com>
2013-12-02 16:46:44 +0200

    the following/follower associations are complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,11 @@ class User < ActiveRecord::Base
   # dependent: :destroy приговаривает связанные микросообщения (т.e., те что принадлежат данному пользователю) быть уничтоженными при уничтожении самого пользователя
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   # before_save { |user| user.email = email.downcase }
   before_save { email.downcase! }
@@ -31,6 +36,21 @@ class User < ActiveRecord::Base
   def feed
     # Это предварительное решение. См. полную реализацию в "Following users".
     Micropost.where("user_id = ?", id)
+  end
+
+  # метод following? принимает пользователя, названного other_user и проверяет, существует ли он в базе данных
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  # метод follow! вызывает create! через relationships ассоциацию для создания взаимоотношения с читаемым
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  # Прекращение слежения за сообщениями пользователя посредством уничтожения взаимоотношения
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private   #All methods defined in a class after private are automatically hidden
