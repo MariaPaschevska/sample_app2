23ac12cadc57733e8e0724bd2b381733f152bdbb
Maria Paschevska <mpasch@softserveinc.com>
2013-12-03 10:59:24 +0200

    adding Ajax to web forms

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,20 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to @user
+    # redirect_to @user  - not used already because of AJAX respond below:   (AJAX reqest - spec/controllers/relation....)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to @user
+    respond_to do |format|   
+    # выполняется только одна из строк (в зависимости от характера запроса)                
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
